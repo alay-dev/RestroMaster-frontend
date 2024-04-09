dbe0612f22ac89b1fd2d7e50f97278f5878c954e
Alay Naru <alay@zocket.com>
2024-04-09 14:15:43 +0530

    baseurl changed

--- config/api.ts
+++ config/api.ts
@@ -1,2 +1,2 @@
-// export const baseurl = "http://43.204.38.228:4000";
-export const baseurl = "http://localhost:4000";
+export const baseurl = "http://13.201.29.114:4000";
+// export const baseurl = "http://localhost:4000";
